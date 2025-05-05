edf2f794ee386f1a9380e0e47280f5d9c19def85
Moses Oghene <mosesogheno@gmail.com>
2025-05-05 09:28:55 +0100

    collect input

--- deitel-ch04-kata/main.go
+++ deitel-ch04-kata/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func collectInput() (float64, float64) {
@@ -20,9 +21,55 @@ func collectInput() (float64, float64) {
 	return mile, gallon
 }
 
-func main() {
+func printTrip(mile float64, gallon float64) {
+	fmt.Printf("%.2f | %.2f\n", mile, gallon)
+}
 
-	mile, galon := collectInput()
-	fmt.Println("Mile:", mile)
-	fmt.Println("Galon:", galon)
+func main() {
+	trips := make(map[float64]float64)
+	for {
+		mile, galon := collectInput()
+		trips[mile] = galon
+		fmt.Print("Enter new trip? (y/n) : ")
+		response, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		response = strings.ToLower(strings.TrimSpace(response))
+		if response == "y" || response == "yes" {
+			continue
+		} else if response == "n" || response == "no" {
+			fmt.Println("Mile | Gallon")
+			fmt.Println("-------------")
+			printTrips(trips)
+			break
+		} else {
+			fmt.Println("Invalid input")
+			continue
+		}
+	}
+
+	var car Car = Car{"Camry", "Totota"}
+
+	fmt.Println(car.Make())
+}
+
+func printTrips(trips map[float64]float64) {
+	for mile, trip := range trips {
+		printTrip(mile, trip)
+	}
+}
+
+type Car struct {
+	model string
+	make  string
+}
+
+func (c Car) Make() string {
+	return c.make
+}
+
+func (c Car) Model() string {
+	return c.model
+}
+
+func (c Car) SetModel(model string) {
+	c.model = model
 }
