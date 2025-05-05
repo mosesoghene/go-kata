18d23bb9f56e15df3c05fa35a433f0a0f8db3b40
Moses Oghene <mosesogheno@gmail.com>
2025-05-05 10:07:40 +0100

    eight

---
+++ deitel-ch04-kata/eight.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	result := 5 / 2
+	precise := 5.0 / 2.0
+	fmt.Println("Integer division:", result)
+	fmt.Println("Floating-point division:", precise)
+}
